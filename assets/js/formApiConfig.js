dd98265285cb390a03d369513d5061ee6cae6161
Marcos Otermin <100133763+Marcos-Oter@users.noreply.github.com>
2023-10-10 18:34:14 -0300

    API Form y optimización de video
    Se agregó la API de Google Sheets para enviar los datos del formulario y se comprimió el video del primer section

---
+++ assets/js/formApiConfig.js
@@ -0,0 +1,3 @@
+const API_URL = "https://sheetdb.io/api/v1/fc9tst40wyrgy";
+
+export { API_URL };
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,25 @@
+
+import { API_URL } from "./formApiConfig.js";
+
 // script.js
 const botonMostrarModal = document.getElementById('mostrarModal');
-const modal = document.getElementById('miModal');
+const modal = document.getElementById('dialog');
 const botonCerrarModal = document.getElementById('cerrarModal');
 const botonCerrarModal2 = document.getElementById('cerrarModal2');
 
 botonMostrarModal.addEventListener('click', () => {
-    modal.style.display = 'block';
+    //modal.style.display = 'block';
+    modal.showModal();
 });
 
 botonCerrarModal.addEventListener('click', () => {
-    modal.style.display = 'none';
+    //modal.style.display = 'none';
+    modal.close();
 });
+
 botonCerrarModal2.addEventListener('click', () => {
-    modal.style.display = 'none';
+   // modal.style.display = 'none';
+    modal.close();
 });
 
 /*window.addEventListener('click', (event) => {
@@ -20,3 +27,33 @@ botonCerrarModal2.addEventListener('click', () => {
         modal.style.display = 'none';
     }
 });*/
+
+document.querySelector("form").addEventListener("submit", async (e) =>
+{
+    e.preventDefault();
+
+    const form = document.querySelector("form");
+
+    let dataForm = new FormData(form);
+
+    dataForm.append("ticket_time", new Date().toString());
+
+    await fetch(API_URL, {
+
+        method:"POST",
+        body: dataForm,
+    });
+
+    form.classList.add("animation");
+
+    alert("Su consulta fue enviada con éxito.");
+
+    const endForm = ()=> { 
+        
+        modal.close();
+        form.classList.remove("animation");
+        form.reset();
+    };
+
+    setTimeout(endForm,1000);
+})
